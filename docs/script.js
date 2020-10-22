563588de9c0810a9bec25baf1693dc5e9d9fb8cb
OlegChuy <misterchuy21@gmail.com>
2020-10-23 01:45:32 +0300

    drop-down list is ready

--- docs/script.js
+++ docs/script.js
@@ -1,31 +1,26 @@
 const menuIcon = document.querySelector('.menu');
-//const models_menu = document.querySelector('.models-menu');
 const extraMenu  = document.querySelector('.extraMenu');
 const nav = document.querySelector('.drop-down-list');
 const li_list = nav.querySelectorAll('li');
 
-li_list.forEach(elem => {
-  elem.addEventListener(('mouseenterr'), () => {
-  	const a = elem.querySelector('a');
+li_list.forEach(li => {
+  const a = li.querySelector('a');
+  
+  li.addEventListener(('mouseover'), () => {
   	if (a.classList.contains('first-nesting-a')) {
-      /*a..background = '';
       a.style.color = '#bb0f0f';
-      a.style.border = '';*/
     } else {
-      /*a.style.background = '#bb0f0f';
+      a.style.background = '#bb0f0f';
       a.style.color = 'white';
-      a.style.border = '1px solid #bb0f0f';*/
+      a.style.border = '1px solid #bb0f0f';
     }
-  })
-})
-
-li_list.forEach(elem => {
-  const a = elem.querySelector('a');
-  elem.addEventListener(('mouseleave'), () => {
-    /*a.style.background = 'blue';*/
-    /*a.style.color = '';
-    a.style.border = '';*/
-  })
+  });
+  
+  li.addEventListener(('mouseleave'), () => {
+    a.style.background = '';
+    a.style.color = '';
+    a.style.border = '';
+  });
 })
 
 menuIcon.addEventListener(('mouseenter'), () => {
@@ -37,14 +32,14 @@ menuIcon.addEventListener(('mouseleave'), () => {
 })
 
 extraMenu.addEventListener(('mouseenter'), () => {
-  if (document.documentElement.clientWidth < '1280') {
+  if (window.innerWidth < '1280') {
     extraMenu.style.display = 'block';
     menuIcon.querySelector('a').style.color = '#bb0f0f';
   }
 })
 
 extraMenu.addEventListener(('mouseleave'), () => {
-  if (document.documentElement.clientWidth < '1280') {
+  if (window.innerWidth < '1280') {
     menuIcon.querySelector('a').style.color = ''; 
     extraMenu.style.display = '';
   }
@@ -52,12 +47,12 @@ extraMenu.addEventListener(('mouseleave'), () => {
 
 const firstNestingLI = extraMenu.querySelectorAll('.first-nesting-a');
 
-if (document.documentElement.clientWidth < '1280') {
+if (window.innerWidth < '1280') {
   firstNestingLI.forEach(elem => elem.classList.remove('first-nesting-a'));
 }
 
 window.addEventListener('resize', () => {
-  if (document.documentElement.clientWidth > '1280') {
+  if (window.innerWidth > '1280') {
     extraMenu.style.display = '';
     firstNestingLI.forEach(elem => elem.classList.add('first-nesting-a'));  
   } else {
